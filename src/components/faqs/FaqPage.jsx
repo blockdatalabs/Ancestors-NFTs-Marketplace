9a5d7a7bec16bf1462de791605d74c09cc8e5115
pulsarforge <omgworkshopfire@gmail.com>
2023-07-26 20:11:42 +0200

    re implement faq

--- src/components/faqs/FaqPage.jsx
+++ src/components/faqs/FaqPage.jsx
@@ -39,7 +39,44 @@ function FaqPage(props) {
 
                             <div className="faq__main flat-tabs">
 
-                           
+                            <Tabs>
+
+                                {
+                                        dataTab.map(idx => (
+                                            <TabPanel key={idx.id} className='content-tab'>
+                                                <div className="content-inner">
+                                                    <div className="flat-accordion row">
+                                                        <div className="col-md-12 col-sm-12">
+                                                        {
+                                                            data.slice(0,4).map(idx => (
+                                                                <Accordion show={idx.show} key={idx.id} title={idx.title} className='flat-toggle h6'>
+                                                                    <p className="toggle-content">{idx.text} </p>
+                                                                </Accordion>
+                                                            ))
+                                                        }
+
+                                                        </div>
+
+                                                        <div className="col-md-6 col-sm-12">
+                                                        {
+                                                            data.slice(4,8).map(idx => (
+                                                                <Accordion show={idx.show} key={idx.id} title={idx.title} className='flat-toggle h6'>
+                                                                    <p className="toggle-content">{idx.text} </p>
+                                                                </Accordion>
+                                                            ))
+                                                        }
+                                                        </div>
+                                                        
+                        
+                                                    </div>
+                                                </div>
+                                            </TabPanel>
+                                        ))
+                                    }
+
+
+                                
+                            </Tabs> 
                             <br/>
                             <br/>
                             <br/>
