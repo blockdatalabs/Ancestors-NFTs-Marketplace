bbabfbe0082682c8b21c31f4860d0631bed73bf1
pulsarforge <omgworkshopfire@gmail.com>
2023-09-20 13:34:16 +0200

    increase delay

--- src/components/Evolutions/Planets.jsx
+++ src/components/Evolutions/Planets.jsx
@@ -33,7 +33,7 @@ const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const getImages = async () => {
   const urlImages = "https://ancestors.pulsarforge.io/api/images";
-  await delay(21000);
+  await delay(24000);
   const resp = await fetch(urlImages, {
     method: "GET",
     headers: { "Content-type": "application/json" },
