41a9b93eabdd52ea4dc67fcbf931ed969c8cb34b
pulsarforge <pulsarforgelabs@gmail.com>
2023-11-09 13:09:26 +0100

    update gallery

--- src/pages/GalleryArt.jsx
+++ src/pages/GalleryArt.jsx
@@ -1,5 +1,10 @@
-import T1 from "../assets/images/gallery/0.jpg";
-
+import T0 from "../assets/images/gallery/0.jpg";
+import T1 from "../assets/images/gallery/1.jpg";
+import T2 from "../assets/images/gallery/2.jpg";
+import T3 from "../assets/images/gallery/3.jpg";
+import T7 from "../assets/images/gallery/7.jpg";
+import T8 from "../assets/images/gallery/7_1.jpg";
+import T9 from "../assets/images/gallery/7_2.jpg";
 function GalleryArt() {
     return(
         <>
@@ -12,15 +17,23 @@ function GalleryArt() {
                 </h4>
             </div>
             <div className="col-md-4">
-                <img src={T1} />
+                <img src={T1} style={{width: "90%"}}/>
             </div>
             <div className="col-md-4">
-                <img src={T1} />
+                <img src={T2} style={{width: "90%"}}/>
             </div>
             <div className="col-md-4">
-                <img src={T1} />
+                <img src={T3} style={{width: "90%"}}/>
             </div>
-
+            <div className="col-md-4">
+                <img src={T7} style={{width: "90%"}}/>
+            </div>
+            <div className="col-md-4">
+                <img src={T8} style={{width: "90%"}}/>
+            </div>
+            <div className="col-md-4">
+                <img src={T9} style={{width: "90%"}}/>
+            </div>
          </div>
         </>
     )
